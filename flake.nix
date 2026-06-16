ec957650359ab37ef3c1751fc5706832585fe224
Yusuke Shibata <yusukeshib@gmail.com>
2026-06-15 22:18:44 -0400

    loop: bump version to 0.2.0 (snapshots/ sensor layout + shellcheck-clean)

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
         loop = pkgs.stdenvNoCC.mkDerivation {
           pname = "loop";
-          version = "0.1.0";
+          version = "0.2.0";
           src = ./.;
 
           nativeBuildInputs = [ pkgs.makeWrapper ];
